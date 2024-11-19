2f468f2c793d7fdbf68842f413d79e829d3eec9c
Harmony Mukolwe <harmonymukolwe@gmail.com>
2024-11-19 12:24:54 +0300

    Creates book card

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./home.css"
+import BookCard from "../../components/book_card/BookCard";
 
 const Home = () => (
-    <p>Home</p>
+    <div>
+        <p>Home</p>
+        <BookCard/>
+    </div>
 )
 
 export default Home;
